1cbcbaac01cde40945a0537213f9a3fcb4a4dd95
Huseyin Uslu <1st.huseyin.uslu@gmail.com>
2021-12-25 12:52:48 +0300

    modified the last project to add a new option.

--- java-101-odev/the-closest-numbers-finder/NumberFinderClass.java
+++ java-101-odev/the-closest-numbers-finder/NumberFinderClass.java
@@ -35,7 +35,10 @@ public class NumberFinderClass {
 
         //print
         number = input.nextInt();
+        //the first and second ways
         printNumbersClosest(array,number);
+        //the third way
+        theThirdApproach(array,number);
     }
 
     //The function that prints the numbers which are the goal of that program.
@@ -53,19 +56,28 @@ public class NumberFinderClass {
 
     //the first approach that finds the number we look for
     private static int getBigNumberClosest(int[] array,int number) {
-        int[] difference = new int[array.length];
-        int theBiggestDifference = number;
+       int[] newArray = new int[array.length + 1];
 
-        for (int i = 0; i < array.length; i++) {
-            difference[i] = array[i] - number;
-        }
+       for(int i = 0;i<array.length;i++){
+           newArray[i] = array[i];
+       }
 
-        for (int dif : difference) {
-            if (dif > 0) {
-                theBiggestDifference = theBiggestDifference < dif ? theBiggestDifference : dif;
-            }
-        }
-        return theBiggestDifference + number;
+       //put our number at the end
+       newArray[newArray.length - 1] = number;
+       //then sort the array in ascent order.
+       Arrays.sort(newArray);
+
+        // if we take our number we query about , it means the index one more time than our number index
+        //is the closest bigger number than our number.
+       for(int i = 0;i<newArray.length;i++){
+           int num = newArray[i];
+
+           if(num == number)
+               return newArray[i+1];
+
+       }
+       //if number returns, it means there isn't any bigger number than the one user wants.
+       return number;
     }
 
     //the second approach that finds the number we look for
@@ -79,6 +91,28 @@ public class NumberFinderClass {
            theSmallestNumber = aR;
        }
        return theSmallestNumber;
+    }
+
+    //the 3th way
+    private static void theThirdApproach(int[] array,int number){
+        int theSmallestNumber = number;
+        int theBiggestNumber = number;
+
+        Arrays.sort(array);
+
+        for(int aR : array){
+            if(aR>number){
+             theBiggestNumber = aR;
+             break;
+            }
+            theSmallestNumber = aR;
+        }
+
+        System.out.println(
+                "" +
+                        "the biggest: " + theBiggestNumber + "\n"
+                +"the smallest: " + theSmallestNumber
+        );
     }
 }
 
